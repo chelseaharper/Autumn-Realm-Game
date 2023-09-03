f1cfa44c549fe3203164864c0ba765c09d4ae398
Chelsea Harper <seashell.smith@gmail.com>
2023-09-03 11:35:49 -0600

    Manual Menus Working!

--- pygame_main.py
+++ pygame_main.py
@@ -8,19 +8,29 @@ import utilities
 from utilities import game_state
 import map_functions
 from time import sleep
-import pygame_menu
-from pygame_menu import themes
+import buttons
+import menus
 
 pygame.init()
 
 screen = pygame.display.set_mode((config.screen_width, config.screen_height))
-game_menu = False
+game_menu = True
+player_menu = False
 
 pygame.display.set_caption("Autumn's Realm")
 map = map_functions.Map()
 map.load_map("map01")
 gameinstance = game.Game(screen, map)
-gameinstance.set_up()
+
+font = pygame.font.Font(None, 50)
+startscreen_text = font.render("Welcome to Autumn's Realm!", True, config.white)
+startscreen_textRect = startscreen_text.get_rect()
+startscreen_textRect.center = (config.screen_width//2, 100)
+
+charclass_text = font.render("Press any key to select a class", True, config.white)
+charclass_textRect = charclass_text.get_rect()
+charclass_textRect.center = (config.screen_width//2, 50)
+
 
 clock = pygame.time.Clock()
 def start_game():
@@ -34,29 +44,30 @@ def start_game():
 def load_game():
     pass
 
-mainmenu = pygame_menu.Menu("Autumn's Realm", 600, 400, theme=themes.THEME_DEFAULT)
-mainmenu.add.button('Play', start_game)
-mainmenu.add.button('Load', load_game)
-mainmenu.add.button('Quit', pygame_menu.events.EXIT)
-arrow = pygame_menu.widgets.LeftArrowSelection(arrow_size = (10, 15))
-
 update_start = pygame.USEREVENT + 0
+update_quit = pygame.USEREVENT + 1
 
 while game_state != utilities.GameState.ENDED:
     clock.tick(50)
     if game_menu == True:
-        events = pygame.event.get()
-        mainmenu.update(events)
-        mainmenu.draw(screen)
-        for event in events:
-            if event.type == pygame.QUIT:
-                exit()
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                     exit()
-            elif event.type == update_start:
-                mainmenu.close(start_game())
-                print("Outside Start Game Function!")
+        screen.blit(startscreen_text, startscreen_textRect)
+        for events in pygame.event.get():
+            if menus.start_button.draw(screen):
+                game_menu = False
+                player_menu = True
+            elif menus.quit_button.draw(screen):
+                utilities.end_game()
+    elif player_menu == True:
+        for events in pygame.event.get():
+            screen.fill(config.black)
+            screen.blit(charclass_text, charclass_textRect)
+            if events.type == pygame.KEYDOWN:
+                if events.key == pygame.K_ESCAPE:
+                    utilities.end_game()
+                else:
+                    gameinstance.set_up()
+                    game_state = utilities.GameState.RUNNING
+                    player_menu = False
     elif game_menu == False and gameinstance.playstate == utilities.PlayState.MENU:
         gameinstance.set_up()
     if gameinstance.playstate == utilities.PlayState.MAP:
